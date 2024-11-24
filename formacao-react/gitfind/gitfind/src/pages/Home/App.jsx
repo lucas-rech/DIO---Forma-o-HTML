e4ceaec0db7eed6b322f6651c20e0ea5e10c5f86
Lucas <lucasrech00@gmail.com>
2024-11-24 15:13:24 -0300

    create: background, input and button

--- formacao-react/gitfind/gitfind/src/pages/Home/App.jsx
+++ formacao-react/gitfind/gitfind/src/pages/Home/App.jsx
@@ -1,10 +1,23 @@
 import Header from "../../components/Header/Header"
 
+import background from "../../assets/background.png"
+
+import './App.css'
+
 function App() {
 
   return (
     <div className="App">
       <Header></Header>
+      <div className="content">
+        <img className="background" src={background} alt="background app" />
+        <div className="info">
+          <div>
+            <input name="user" placeholder="@username"></input>
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

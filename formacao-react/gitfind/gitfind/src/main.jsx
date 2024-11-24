c2de0b81d3dcf01ba95b2a3eae5dd7d7497cefd9
Lucas <lucasrech00@gmail.com>
2024-11-24 00:15:04 -0300

    clean project

--- formacao-react/gitfind/gitfind/src/main.jsx
+++ formacao-react/gitfind/gitfind/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './src/Home/App.jsx'
+import App from './pages/Home/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

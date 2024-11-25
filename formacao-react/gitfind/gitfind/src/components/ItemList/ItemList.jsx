f2725c374f4aaa2d02cfb56c0bf3c76868bed3d5
Lucas <lucasrech00@gmail.com>
2024-11-24 22:42:01 -0300

    create: list of item component

---
+++ formacao-react/gitfind/gitfind/src/components/ItemList/ItemList.jsx
@@ -0,0 +1,13 @@
+import './ItemList.css'
+
+const ItemList = ({title, description}) => {
+  return (
+    <div className="item-list">
+        <strong>{title}</strong>
+        <p>{description}</p>
+        <hr />
+    </div>
+  )
+}
+
+export default ItemList
--- formacao-react/gitfind/gitfind/src/pages/Home/App.jsx
+++ formacao-react/gitfind/gitfind/src/pages/Home/App.jsx
@@ -1,8 +1,10 @@
 import Header from "../../components/Header/Header"
+import ItemList from "../../components/ItemList/ItemList"
 
 import background from "../../assets/background.png"
 
 import './App.css'
+
 
 function App() {
 
@@ -15,6 +17,22 @@ function App() {
           <div>
             <input name="user" placeholder="@username"></input>
             <button>Buscar</button>
+          </div>
+          <div className="profile">
+            <img className="profile-photo" src="https://avatars.githubusercontent.com/u/140991738?s=400&u=02b59f6ec52d1d20fcc861e027a2fcc361547ab1&v=4" alt="profile image" />
+            <div>
+              <h3>Lucas Rech</h3>
+              <span>@lucas-rech</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h4>Repositórios</h4>
+            <ItemList title={"Teste 1"} description={"lorem ipsum"}/>
+            <ItemList title={"Teste 2"} description={"lorem ipsum"}/>
+            <ItemList title={"Teste 3"} description={"lorem ipsum"}/>
+            <ItemList title={"Teste 4"} description={"lorem ipsum"}/>
           </div>
         </div>
       </div>
